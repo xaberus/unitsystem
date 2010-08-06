eafa483c1109da4ef2aa01c3bf55df426979a07c
Pavel S <pa_s@mail.ru>
2010-08-06 11:39:02 +0200

    use bt_log if compiled with TEST

--- unitsystem.c
+++ unitsystem.c
@@ -2,7 +2,23 @@
 #include "unitsystem.h"
 
 #ifdef TEST
-err_log_t * err_logger;
-void * err_logger_data;
+#include <stdarg.h>
+#include <stdio.h>
+
+void test_logger(void * data, char format[], ...) {
+	va_list ap;
+	char buff[4096];
+	UNUSED_PARAM(data);
+
+	va_start(ap, format);
+	vsnprintf(buff, 4096, format, ap);
+	va_end(ap);
+
+	bt_log("%s", buff);
+}
+
+
+err_log_t * err_logger = test_logger;
+void * err_logger_data = NULL;
 #endif
 
